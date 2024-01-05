d125c67ddbba1e4439ba81f036b73737745be197
jayardeshana <jay.a@brilworkss.com>
2024-01-05 13:20:41 +0530

    Revert "implement login with facebook"
    This reverts commit b34836e327e0b33a88159cf9a70ac26dd635f657.

--- src/main/java/com/brilworkstask/social_login/repository/UserRepository.java
+++ src/main/java/com/brilworkstask/social_login/repository/UserRepository.java
@@ -2,13 +2,6 @@ package com.brilworkstask.social_login.repository;
 
 import com.brilworkstask.social_login.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    @Query("SELECT u.referenceId FROM User u")
-    List<Long> findReferenceIds();
-
 }
